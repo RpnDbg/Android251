f42a8769b45887236b522947b106aa56a5c505e3
rpndb <rdabbaghian@wccnet.edu>
2024-02-18 00:49:04 -0500

    new

---
+++ assignments/a3/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val enteredNames = mutableListOf<String>()
+    private val ENTERED_NAMES_KEY = "enteredNamesKey"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+
+            val enteredText = binding.editTextText.text.toString()
+
+            if (enteredText.isNotBlank()) {
+
+                enteredNames.add(enteredText)
+
+                displayEnteredNames()
+            } else {
+                binding.textView.text = "No Name Entered"
+            }
+        }
+        savedInstanceState?.let {
+            enteredNames.addAll(it.getStringArrayList(ENTERED_NAMES_KEY) ?: emptyList())
+            displayEnteredNames()
+        }
+    }
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putStringArrayList(ENTERED_NAMES_KEY, ArrayList(enteredNames))
+        super.onSaveInstanceState(outState)
+    }
+
+    private fun displayEnteredNames() {
+        val namesString = enteredNames.joinToString(", ")
+
+        binding.textView.text = namesString
+    }
+}
